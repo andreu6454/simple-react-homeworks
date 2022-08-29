46e60888975a1f1a801ea256993c8d8333b6ae0e
Andrey Malin <andreu6454@gmail.com>
2022-08-29 18:40:37 +0300

    Updates

---
+++ precache-manifest.87e7a76535ce8f960a5b08da6fafb0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae62ee0332d222c5e9e5fa5967a0ebdc",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "fceba25f171dfdd7439d",
+    "url": "/simple-react-homeworks/static/css/main.f9ca05fa.chunk.css"
+  },
+  {
+    "revision": "4a3639009c606eb7c9af",
+    "url": "/simple-react-homeworks/static/js/2.c15bddfb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/simple-react-homeworks/static/js/2.c15bddfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fceba25f171dfdd7439d",
+    "url": "/simple-react-homeworks/static/js/main.b3d43fe8.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
